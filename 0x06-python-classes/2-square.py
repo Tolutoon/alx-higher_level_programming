e714a67dd9b24d1e3f3366cc733b8e98ba1e8405
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-12-21 03:33:33 +0100

    third task documentation 😐😐😐

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 
-"""Defining a class called Square."""
+"""Define a class Square."""
 
 
 class Square:
-    """Represent a Square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initializing a new square.
-
-        Args: 
-            size (int): The size of the new square
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
