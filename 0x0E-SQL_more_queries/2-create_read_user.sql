990b7b5838eddca57624c1d69eb3679c067c3d12
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-02-16 21:19:28 +0100

    task documentation

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- creaates a user
+-- creates a user
 CREATE USER IF NOT EXITS user_0d_2@localhost IDENTIFIED BY 'user_od_2-pwd';
 -- grants SELECT privileges to a user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
