31993bd350704604726264126ea0435e400957d7
Tolutoon <getolopadetolu@gmail.com>
2023-02-16 12:10:59 -0800

    third task documentation

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,6 @@
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creaates a user
-CREATE USER IF NOT EXITS user_0d_2@localhost IDENTIFIED BY 'user_od_2-pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_od_2-pwd';
 -- grants SELECT privileges to a user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
-
