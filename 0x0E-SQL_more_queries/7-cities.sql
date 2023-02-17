9759d63361a933ef08eef21e7b5e4a40845b2103
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-02-17 18:19:59 +0100

    eight task documentation

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- creates the database hbtn_0d_usa and the table cities on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT),
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFRENCES states(id));
