d77e04a993ac05c73a00b8c59ac677e7f51cf8b8
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-03-16 09:40:59 +0100

    fifth task documentation

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 class Rectangle {
-  constructor (w, h)
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let r = '';
+      for (let j = 0; j < this.width; j++) {
+        r += 'X';
+      }
+    console.log(r);
+    }
+  }
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
