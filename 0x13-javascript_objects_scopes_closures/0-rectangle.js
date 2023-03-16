6fceb345276d328845c5665380330214350b494f
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-03-16 09:19:22 +0100

    task 0 fix

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 class Rectangle {
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> 1d2e15e6fa01f1e36361c6c54897c8e98b115671
 module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+class Rectangle {
+  constructor (w, h)
