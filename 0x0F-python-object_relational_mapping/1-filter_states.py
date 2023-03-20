fa5c08be8613e36e5e85749649aa57b2f501fc68
Tolutoon <getolopadetolu@gmail.com>
2023-03-19 17:23:12 -0700

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" List all states with a name starting with N from the database hbtn_0e_0_usa. """
+""" List all states with a name starting with N from the database. """
 
 import sys
 import MySQLdb
