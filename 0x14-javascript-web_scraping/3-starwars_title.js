4cdd1c39b3a00285ef14bba600144a98a6d8829a
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-04-25 20:58:59 +0100

    fourth task documentation

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const episodeNum = process.argv[2];
-const API_URL = 'https://swap-api.hbtn.io/api/films/';
+const API_URL = 'https://swap-api.alx-tools.com/api/films/';
 
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
